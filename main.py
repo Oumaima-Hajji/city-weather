88760fbe581305a97a890c5ef92fe001f6068cca
Oumaima Hajji <oumhjj@gmail.com>
2022-12-08 14:02:17 +0100

    updated main function

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from utils import *
 
 
 if __name__ == "__main__":
-    weather_notifier(city="Paris", country="France",
-                     phone_number="write phone number")
+    send_message_people()
+    
