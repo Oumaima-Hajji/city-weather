0e1136bb4f873c2c9c19ddf7e2cc59e49c85f097
Oumaima Hajji <oumhjj@gmail.com>
2022-12-08 14:05:06 +0100

    updated all functions

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# city-weather
+# Get a city's weather information using a weather forcast API and a forward geocoding API
+
+## Project description
+
+This project gives us a city's temperature and rain rate at the same day by only giving the city's name and corresponding coutry. 
+
+
+The project uses two functions.
+
+
+
+The main function (cityweather) gives the weather information of a city, the date of the day, the hour, the temperature and the rain rate. It only needs the city's name and country. 
+
+It's able to do so by using the other function (cityapi) that takes the city name and country and returns the latitude and langitude of the city. This information is then used by the main function to apoint the city.
+
+
+- "cityweather" gives the city's eather information by using [Open-Meteo](https://open-meteo.com/en/docs#latitude=34.96&longitude=-5.44&hourly=pressure_msl,visibility), a weather API. This API let's you select a city but instead of using the name of the city, it uses the city's latitude and longitude.
+
+- "cityapi" gives the city's latitude and longitude by giving it the city's name and country by using [API Ninjas](https://api-ninjas.com/api/geocoding), a geocoding API that enables you to convert any city from any country to latitude and longitude coordinates.
+
+
--- utils.py
+++ utils.py
@@ -12,15 +12,14 @@ from googleapiclient.errors import HttpError
 import requests
 import emoji
 from code_dict import code_dict
-from connectsheet import read_google_sheet
 
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+# If modifying these scopes, delete the file secrets/token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = "12Iy5vNJyVi9aG3TokPRnPfISCNffI4Ig1_7gax5Mavw"
-SAMPLE_RANGE_NAME = "people_records!A1:E10"
+SAMPLE_SPREADSHEET_ID = 'SAMPLE_SPREADSHEET_ID'
+SAMPLE_RANGE_NAME = 'people_records!A1:E10'
 
 
 def get_lat_long(city: str, country: str):
@@ -31,7 +30,7 @@ def get_lat_long(city: str, country: str):
     """
     response = requests.get(
         url=f"https://api.api-ninjas.com/v1/geocoding?city={city}&country={country}",
-        headers={"X-Api-Key": "1c0/d/JhFTx7iJPB6vN+NA==Eh281hVfSYXLVh7H"},
+        headers={"X-Api-Key": "NinjasAPI Key"},
     )
 
     data_city = response.json()
@@ -66,11 +65,11 @@ def get_weather_description(chosen_key: str):
 
 
 def send_sms(message: str, phone_number: str):
-    """
+    '''
     Function that sends an sms text using a SMS sender API.
     Input : message : str , phone_number:str
     Output : Status of post-call
-    """
+    '''
     request = requests.post(
         "https://textbelt.com/text",
         {
@@ -134,44 +133,49 @@ def read_google_sheet():
     Output : people_record : dict
     """
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
+    # The file secrets/token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists('secrets/token.json'):
+        creds = Credentials.from_authorized_user_file('secrets/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'secrets/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open('secrets/token.json', 'w') as token:
             token.write(creds.to_json())
 
     try:
-        service = build("sheets", "v4", credentials=creds)
+        service = build('sheets', 'v4', credentials=creds)
 
         # Call the Sheets API
         sheet = service.spreadsheets()
-        result = (
-            sheet.values()
-            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-            .execute()
-        )
-        values = result.get("values", [])
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
 
         if not values:
-            print("No data found.")
+            print('No data found.')
             return
 
         people_record = {
+
             values[0][0]: [values[1][0], values[2][0], values[3][0]],
+
             values[0][1]: [values[1][1], values[2][1], values[3][1]],
+
             values[0][2]: [values[1][2], values[2][2], values[3][2]],
+
             values[0][3]: [values[1][3], values[2][3], values[3][3]],
-            values[0][4]: [values[1][4], values[2][4], values[3][4]],
+
+            values[0][4]: [values[1][4], values[2][4], values[3][4]]
+
+
         }
 
     except HttpError as err:
@@ -181,22 +185,18 @@ def read_google_sheet():
 
 
 def send_message_people():
-    """
-    Function that sends to our subscribers a weather notification.
+    '''
+    Function that sends to our subscribers a weather notification. 
     Output : message notification :str
 
-    """
-    people_record, total_people = read_google_sheet(), len(read_google_sheet()["name"])
+    '''
+    people_record, total_people = read_google_sheet(), len(
+        read_google_sheet()['name'])
 
     for item in range(total_people):
-        name, city, country, call_sign, phone_number = (
-            people_record["name"][item],
-            people_record["city"][item],
-            people_record["country"][item],
-            people_record["call_sign"][item],
-            people_record["phone_number"][item],
-        )
+        name, city,  country, call_sign, phone_number = people_record['name'][item], people_record['city'][
+            item], people_record['country'][item], people_record['call_sign'][item], people_record['phone_number'][item]
 
-        phone = "+" + call_sign + " " + phone_number
+        phone = '+' + call_sign + ' ' + phone_number
 
         weather_notifier(city=city, country=country, phone_number=phone)
